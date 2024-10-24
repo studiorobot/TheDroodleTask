d33aca8b4ed853d8265211f19724ad73ef8ddc34
dtrey-umich <dtrey@umich.edu>
2024-10-24 11:52:26 -0400

    Eliza Delay
    I added a random delay into eliza

--- conversationManagement/elizaConversation/elizaConversation.py
+++ conversationManagement/elizaConversation/elizaConversation.py
@@ -1,7 +1,8 @@
 from ..standardConversation.standardConversation import standardConversation #parent 
-from ..conversationTools import encodeMessage
-from ..eliza.eliza import Eliza
-import os
+from ..conversationTools import encodeMessage #message encoder
+from ..eliza.eliza import Eliza #eliza response system https://github.com/wadetb/eliza
+import time #for delay function
+import random #randomness for delay function
 
 class elizaConversation(standardConversation):
     def __init__(self, conversationName: str, savePath: str = 'conversationArchive'):
@@ -11,6 +12,9 @@ class elizaConversation(standardConversation):
         #if model is allowed to default, use the model given in class init
         if model is not None:
             super()._makeRequest(tempConversation, model)
+
+        #DELAY
+        time.sleep(random.uniform(1, 3))
 
         #If conversation is allowed to default, use the conversation in the class instance
         if tempConversation is None:
